3d9da8fc84b838ce3f710afc3c2d63c839c2316b
Alex Hasapis <ahasapis@hubbell.com>
2021-04-15 14:08:58 -0400

    Add project files.

---
+++ Controllers/ComicBooksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ContentResult Detail()
+        {
+            return new ContentResult()
+            {
+                Content = "Hello from the comic books controller!"
+            };      
+        }
+    }
+}
